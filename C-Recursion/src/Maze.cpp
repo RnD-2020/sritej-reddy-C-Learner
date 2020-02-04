0827467fdce723c879434a4274ab5235ba19437a
Sritej Reddy <chilkurisritej@gmail.com>
2020-02-04 18:57:50 +0530

    made maze a little easier to undestand

--- C-Recursion/src/Maze.cpp
+++ C-Recursion/src/Maze.cpp
@@ -40,21 +40,17 @@ bool found = false;
 
 int exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2)
 {
-	if ((x1 < rows && y1 < columns) && (x1>= 0 && y1 >= 0))
+	if (x1 < rows && y1 < columns && x1>= 0 && y1 >= 0)
 	{
-		if (x1 == x2 && y1 == y2 && *(maze + columns*x1 + y1) == 1)
-		{
+		if (x1 == x2 && y1 == y2) { //if found the position this gets true
 			found = true;
-			return 1;
-		}
-		else if (x1 == x2 && y1 == y2 && *(maze + columns*x1 + y1) == 0)
-		{
-			found = true;
+			if(*(maze + columns*x1 + y1) == 1) //if path exists till destination return 1
+				return 1;
 			return 0;
 		}
 		else if (*(maze + columns*x1 + y1) == 1)
 		{
-			*(maze + columns*x1 + y1) = 2;
+			*(maze + columns*x1 + y1) = 2; //we set every maze[][] to 2 so that if we once again visit that ele we can ignore it
 			if (exists(maze, rows, columns, x1 + 1, y1, x2, y2) == 1)
 				return 1;
 			if (found) //this becomes true when a 0 exits at the destination position
